ab468d2f8d3bda8dd9a14de30ba23bf2ccbf314b
Umma Ahimsha <uahimsha@gmail.com>
2021-01-11 15:59:32 +0700

    added BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import './index.css';
 
 import App from './App';
 
-
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <Router>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
