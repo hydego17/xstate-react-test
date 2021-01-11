9ed1ebb50e48ba1470fa699871035c3ace1f81ba
Umma Ahimsha <uahimsha@gmail.com>
2021-01-11 15:34:37 +0700

    move to covid folder

---
+++ src/machine/covid-19/covidMachine.js
@@ -0,0 +1,82 @@
+import { Machine, assign, spawn } from "xstate";
+import { createCovidDataMachine } from "./covidDataMachine";
+
+const urlCountries = "https://covid19.mathdro.id/api/countries";
+
+const fetchCountriesState = {
+  initial: "loading",
+  states: {
+    loading: {
+      invoke: {
+        id: "fetch-countries",
+        src: "getListCountries",
+        onDone: {
+          target: "loaded",
+          actions: assign({
+            listCountries: (_context, event) => event.data.countries
+          })
+        },
+        onError: {
+          target: "failure"
+        }
+      }
+    },
+    loaded: {
+      type: "final"
+    },
+    failure: {
+      on: {
+        RETRY: "loading"
+      }
+    }
+  }
+};
+
+export const covidMachine = Machine(
+  {
+    id: "covid-machine",
+    initial: "idle",
+    context: {
+      listCountries: [],
+      statistics: {},
+      countryStat: null
+    },
+    states: {
+      idle: {
+        ...fetchCountriesState
+      },
+      selected: {}
+    },
+    on: {
+      SELECT: {
+        target: ".selected",
+        actions: assign((context, event) => {
+          let countryStat = context.statistics[event.name];
+
+          if (countryStat) {
+            return {
+              ...context,
+              countryStat
+            };
+          }
+
+          countryStat = spawn(createCovidDataMachine(event.name));
+          return {
+            ...context,
+            statistics: {
+              ...context.statistics,
+              [event.name]: countryStat
+            },
+            countryStat
+          };
+        })
+      }
+    }
+  },
+  {
+    services: {
+      getListCountries: (_context, _event) =>
+        fetch(urlCountries).then(response => response.json())
+    }
+  }
+);
