04c395407d6f556b4a4bd8ab6ad44fdae67a5f1c
Umma Ahimsha <uahimsha@gmail.com>
2021-01-11 16:16:55 +0700

    + todos page

---
+++ src/pages/todos/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+import { Heading } from '@chakra-ui/react'
+
+import { PageContainer } from '../../components/PageContainer'
+
+export default function TodosPage() {
+    return (
+        <PageContainer title="todos"> 
+            
+        </PageContainer>
+    )
+}
+
