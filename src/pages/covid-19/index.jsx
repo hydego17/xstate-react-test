9dd0f2dde2e54e3196e20227f1c7c875161dd21d
Umma Ahimsha <uahimsha@gmail.com>
2021-01-11 15:34:55 +0700

    move to covid folder

---
+++ src/pages/covid-19/index.jsx
@@ -0,0 +1,54 @@
+import React, { createContext } from 'react';
+import { useMachine } from '@xstate/react';
+
+import { Box, Container, Flex, Heading, Select, Code } from '@chakra-ui/react';
+
+import { covidMachine } from '../../machine/covid-19/covidMachine';
+import Indicator from './Indicator';
+
+export const MachineProvider = createContext();
+
+export default function CovidPage() {
+  const [current, send] = useMachine(covidMachine);
+  const { countryStat } = current.context;
+
+  return (
+    <MachineProvider.Provider value={countryStat}>
+      <Box mt={6}>
+        <Flex direction="column" align="center">
+          {/* Header */}
+
+          <Box as="header" pt={8}>
+            <Heading as="h2" size="lg">xstate covid</Heading>
+          </Box>
+
+          {/* state display */}
+
+          <Box py={6}>
+            <Code>
+              {' '}
+              {covidMachine.id} state: {current.toStrings().join(' ')}{' '}
+            </Code>
+          </Box>
+
+          {/* Country Select */}
+
+          <Box maxW="xl">
+            <Select
+              onChange={(e) => send('SELECT', { name: e.target.value })}
+              placeholder="Select Country"
+            >
+              {current.context.listCountries.map((country) => {
+                return <option key={country.name}>{country.name}</option>;
+              })}
+            </Select>
+          </Box>
+
+          {/* Indicator */}
+
+          <Box py={6}>{countryStat && <Indicator />}</Box>
+        </Flex>
+      </Box>
+    </MachineProvider.Provider>
+  );
+}
