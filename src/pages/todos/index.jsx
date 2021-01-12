95dd15ca6d63905bab77a51037f62e1ec3a52c91
Umma Ahimsha <uahimsha@gmail.com>
2021-01-12 09:32:27 +0700

    refactored

--- src/pages/todos/index.jsx
+++ src/pages/todos/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
+import { Box, Input } from '@chakra-ui/react';
 
-import { Heading } from '@chakra-ui/react'
-
-import { PageContainer } from '../../components/PageContainer'
+import { PageContainer } from '../../components/PageContainer';
 
 export default function TodosPage() {
-    return (
-        <PageContainer title="todos"> 
-            
-        </PageContainer>
-    )
+  return (
+    <PageContainer title="todos">
+      <Box py={4}>
+        <Input type="text" placeholder="What needs to be done?" autoFocus />
+      </Box>
+    </PageContainer>
+  );
 }
-
